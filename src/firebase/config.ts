0b2b38cd514a4c2459545a614f29c8a287b7b051
Sav Serena <savserena@gmail.com>
2025-11-06 19:08:58 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "venuevendors3",
+  "appId": "1:547901537563:web:774caa4f6d53a63e51a7be",
+  "apiKey": "AIzaSyDs132vUlvpqVcB0_bIczutDUOZDafeh6g",
+  "authDomain": "venuevendors3.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "547901537563"
+};
