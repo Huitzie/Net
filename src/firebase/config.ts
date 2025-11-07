81e28fca644ac11475d56a98c0b1c942301e2856
Sav Serena <savserena@gmail.com>
2025-11-07 00:46:12 +0000

    is this right: Then, initialize Firebase and begin using the SDKs for th

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,6 +3,7 @@ export const firebaseConfig = {
   "appId": "1:547901537563:web:774caa4f6d53a63e51a7be",
   "apiKey": "AIzaSyDs132vUlvpqVcB0_bIczutDUOZDafeh6g",
   "authDomain": "venuevendors3.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "547901537563"
+  "storageBucket": "venuevendors3.appspot.com",
+  "messagingSenderId": "547901537563",
+  "measurementId": ""
 };
